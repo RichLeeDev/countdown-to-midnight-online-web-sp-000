394381eaad0cd64a4b187f636541af5b5f95a4e0
Richard Lee <richlee81192@gmail.com>
2019-09-17 19:18:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,3 +11,12 @@ end
 end
 
 def countdown_with_sleep(number)
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep 2
+  if number == 0 
+    return "HAPPY NEW YEAR!"
+  end
+end
+end 
