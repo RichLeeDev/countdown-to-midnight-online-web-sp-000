a77e47df3494c29f5092b3634f9ff49138cdac5b
Richard Lee <richlee81192@gmail.com>
2019-09-17 19:16:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,10 +4,8 @@ def countdown(number)
   counter = number
   while counter > 0 
   puts "#{number} SECOND(S)!"
-  sleep 2
   counter -= 1
   if counter == 0 
-    
+    return "HAPPY NEW YEAR!"
 end
-puts "HAPPY NEW YEAR!"
 end
